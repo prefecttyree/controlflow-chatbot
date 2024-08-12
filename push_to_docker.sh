5c755bdd38c7648b9b6eb1153a0a882b839c998a
ty-the-prefectionist <tyree@prefect.com>
2024-08-11 21:58:15 -0500

    updating dockerhub

--- push_to_docker.sh
+++ push_to_docker.sh
@@ -10,14 +10,14 @@ GIT_HASH=$(git rev-parse --short HEAD)
 docker build -t controlflow-chatbot .
 
 # Tag the image with the git hash
-docker tag controlflow-chatbot $DOCKERHUB_USERNAME/gcr.io/$GCP_PROJECT_ID/k8s-streamlit:$GIT_HASH
+docker tag controlflow-chatbot $DOCKERHUB_USERNAME/controlflow-chatbot: $GIT_HASH
 
 # Log in to Docker Hub
 echo "Please enter your Docker Hub password:"
 docker login --username $DOCKERHUB_USERNAME
 
 # Push the image to Docker Hub
-docker push $DOCKERHUB_USERNAME/gcr.io/$GCP_PROJECT_ID/k8s-streamlit:$GIT_HASH
+docker push $DOCKERHUB_USERNAME/controlflow-chatbot:$GIT_HASH
 
 echo "Image pushed successfully to Docker Hub with tag: $GIT_HASH"
 
